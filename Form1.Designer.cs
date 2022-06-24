6201d776a67626bb431137f34fd29e0b27808780
Dan Smestad <hockeydan25@gmail.com>
2022-06-24 11:34:37 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace TaxCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTaxIncome = new System.Windows.Forms.TextBox();
+            this.txtTaxOwed = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.Snow;
+            this.btnCalculate.Location = new System.Drawing.Point(85, 337);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.ForeColor = System.Drawing.Color.DimGray;
+            this.btnExit.Location = new System.Drawing.Point(321, 337);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.818182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Taxable Income:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.818182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(82, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Income Tax Owed:";
+            // 
+            // txtTaxIncome
+            // 
+            this.txtTaxIncome.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTaxIncome.Location = new System.Drawing.Point(296, 126);
+            this.txtTaxIncome.Name = "txtTaxIncome";
+            this.txtTaxIncome.Size = new System.Drawing.Size(100, 20);
+            this.txtTaxIncome.TabIndex = 1;
+            // 
+            // txtTaxOwed
+            // 
+            this.txtTaxOwed.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtTaxOwed.Location = new System.Drawing.Point(296, 227);
+            this.txtTaxOwed.Name = "txtTaxOwed";
+            this.txtTaxOwed.ReadOnly = true;
+            this.txtTaxOwed.Size = new System.Drawing.Size(100, 20);
+            this.txtTaxOwed.TabIndex = 0;
+            this.txtTaxOwed.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(529, 417);
+            this.Controls.Add(this.txtTaxOwed);
+            this.Controls.Add(this.txtTaxIncome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Income Tax Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTaxIncome;
+        private System.Windows.Forms.TextBox txtTaxOwed;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaxCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    
+        
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal income = Convert.ToDecimal(txtTaxIncome.Text);
+            decimal taxowed = .0m;           // sets tax[percent variable and decimal value
+            decimal taxpercent = .0m;        // sets tax[percent variable and decimal value
+            decimal incometax = .0m;         // sets tax[percent variable and decimal value
+            decimal over = .0m;
+            decimal overtax = .0m;
+
+            if (income > 0 && income <= 9875) // begigning nested else if clause
+                taxpercent = .10m;
+                 
+            else if (income > 9876 && income <= 40125)      //income range to setup taxpercent,taxowed and over.
+            {   taxpercent = .12m;                          // tax percent for range setup by the income input.
+                taxowed = 987.50m;                          // taxowed is a static value. 
+                over = 9875m;}
+            else if (income > 40126 && income <= 85525)
+            {   taxpercent = .22m;                          // tax percent for range setup by the income input.
+                taxowed = 4617.50m;                         // taxowed is a static value. 
+                over = 40125;}                              // over is a static value.
+            else if(income > 85526 && income <= 163300)
+            {   taxpercent = .24m;                          // tax percent for range setup by the income input.
+                taxowed = 14605.50m;                        // taxowed is a static value.
+                over = 85525;}                             // over is a static value.
+            else if (income > 163301 && income <= 207350)
+            {   taxpercent = .32m;                          // tax percent for range setup by the income input.
+                taxowed = 33271.50m;                        // taxowed is a static value.
+                over = 163300;}                             // over is a static value.
+            else if (income > 207351 && income <= 518400)
+            {   taxpercent = .35m;                          // tax percent for range setup by the income input.
+                taxowed = 47367.50m;                        // taxowed is a static value.
+                over = 207350;}                             // taxowed is a static value.
+            else                                            // end nested else if clause 
+            {   taxpercent = .37m;                          // tax percent for range setup by the income input.
+                taxowed = 156235m;                          // taxowed is a static value.
+                over = 518400;}                             // over is a static value.
+
+            overtax = (taxpercent * income) - (taxpercent * over); // calcutale and assign value for the varibles
+            incometax = taxowed + overtax;  // calcutale and assign value for the varibles 
+
+            txtTaxOwed.Text = incometax.ToString("c");  // text box will have a curency value output.
+                    
+            txtTaxIncome.Focus();          //returns control to the  tax income TextBox.  
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();                                     // closes the propgram to exit.
+        }
+    }
+
+}
